96082077dbd230483979f3c6c8d1f52d0cb16513
alara <alara@clarika.com.ar>
2024-03-08 17:51:10 -0300

    telescope power

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -25,33 +25,48 @@ return {
 
 			require("telescope").setup({
 				defaults = {
-					layout_strategy = "vertical",
+					layout_strategy = "center",
 					path_display = { "absolute" },
+					layout_config = {
+						mirror = true,
+						horizontal = {
+							promp_position = "bottom",
+						},
+					},
+					sorting_strategy = "ascending",
 					mappings = {
 						i = {
 							["<c-k>"] = telescope_actions.move_selection_previous,
 							["<c-j>"] = telescope_actions.move_selection_next,
+							["<c-p>"] = require("telescope.actions.layout").toggle_preview,
+							["<a-j>"] = telescope_actions.preview_scrolling_down,
+							["<a-k>"] = telescope_actions.preview_scrolling_up,
+							["<space>"] = telescope_actions.select_default,
+						},
+						n = {
+							["<c-p>"] = require("telescope.actions.layout").toggle_preview,
+							["<a-j>"] = telescope_actions.preview_scrolling_down,
+							["<a-k>"] = telescope_actions.preview_scrolling_up,
+							["<space>"] = telescope_actions.select_default,
 						},
 					},
-				},
-				pickers = {
-					find_files = {
-						previewer = false,
-					},
-					buffers = {
-						previewer = false,
-						path_display = { "absolute" },
-					},
-					oldfiles = {
-						previewer = false,
-					},
-					file_browser = {
-						layout_strategy = "horizontal",
+					preview = {
+						hide_on_startup = true,
 					},
 				},
 				extensions = {
 					file_browser = {
-						hijack_netrw = true,
+						initial_mode = "normal",
+						mappings = {
+							n = {
+								["<c-b>"] = require("telescope._extensions.file_browser.actions").goto_parent_dir,
+								["<c-r>"] = require("telescope._extensions.file_browser.actions").goto_cwd,
+								["<c-v>a"] = require("telescope._extensions.file_browser.actions").toggle_hidden,
+								["<c-f>"] = require("telescope._extensions.file_browser.actions").toggle_browser,
+								["<c-t>"] = require("telescope._extensions.file_browser.actions").toggle_all,
+								["<c-o>"] = require("telescope._extensions.file_browser.actions").open,
+							},
+						},
 					},
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
@@ -63,6 +78,7 @@ return {
 			pcall(require("telescope").load_extension, "fzf")
 			pcall(require("telescope").load_extension, "ui-select")
 
+			local file_browser_cmd = ":Telescope file_browser path=%:p:h select_buffer=true<cr>"
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
@@ -72,15 +88,9 @@ return {
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-			vim.keymap.set("n", "<leader>sb", ":Telescope file_browser<cr>", { desc = "[F]ile Browser" })
-			vim.keymap.set(
-				"n",
-				"<leader>ss",
-				":Telescope file_browser path=%:p:h select_buffer=true<cr>",
-				{ desc = "[F]ile Browser Folder" }
-			)
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			vim.keymap.set("n", "<c-s>", file_browser_cmd, { desc = "[F]ile Browser Folder" })
 		end,
 	},
 	{
