0fe0969282446892118fa18b0e571fe5cb691ae9
adrianlara24 <adrianlara24@gmail.com>
2025-02-16 19:00:23 -0300

    add oil

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -12,10 +12,47 @@ return {
       })
 
       local keymap = vim.keymap
-      
+
 
       keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
       keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+    end,
+  },
+  --VIM: OIL
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    lazy = false,
+    config = function()
+      local oil = require('oil')
+      oil.setup({
+        use_default_keybindings = false,
+        view_options = {
+          show_hidden = true,
+        },
+        float = {
+          padding = 0,
+          max_width = 0.8,
+          max_height = 0.8,
+          border = "single",
+          preview_split = "auto",
+          win_options = {
+            winblend = 0,
+          }
+        },
+        keymaps = {
+          ["q"] = "actions.close",
+          ["<space>"] = "actions.select",
+          ["<C-r>"] = "actions.open_cwd",
+          ["<C-p>"] = "actions.preview",
+          ["<A-1>"] = { "actions.select", opts = { vertical = true } },
+          ["<A-2>"] = { "actions.select", opts = { horizontal = true } },
+          ["?"] = "actions.show_help",
+        },
+      })
+      -- vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open oil" })
+      vim.keymap.set("n", "<leader>o", function() oil.open_float() end, { desc = "Open oil" })
     end,
   },
 }
