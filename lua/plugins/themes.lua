027fff3b5cf7eb469e24e9b57ca29d731aa91e00
adrianlara24 <adrianlara24@gmail.com>
2025-04-21 00:04:28 -0300

    update themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -6,24 +6,6 @@ return {
 		opts = {},
 	},
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = true,
-				default_integrations = true,
-				integrations = {
-					cmp = true,
-					nvimtree = true,
-					treesitter = true,
-					gitsigns = true,
-				},
-			})
-		end,
-	},
-	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		config = function()
@@ -35,37 +17,6 @@ return {
 					italic = true,
 					transparency = true,
 				},
-			})
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"savq/melange-nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"marko-cerovac/material.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"kdheepak/monochrome.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"maxmx03/fluoromachine.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			local fm = require("fluoromachine")
-			fm.setup({
-				glow = true,
 			})
 		end,
 	},
